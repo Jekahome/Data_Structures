971ee35923cb7022d6dc705e0f7a729d31e5706b
Eugene Yaroshenko <yaroshjeka@gmail.com>
2021-06-18 17:15:40 +0300

    add link

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ fn main() {
     println!("Linked list: $cargo run --example linked-list-enum");
     println!("Linked list: $cargo run --example linked-list-generic");
 
+    // Переписать реализации циклических структур используя Rc<RefCell<T>> и Weak
+    // https://ricardomartins.cc/2016/06/08/interior-mutability
 }
 
 // бинарный поиск https://shane-o.dev/blog/binary-search-rust-part-2
